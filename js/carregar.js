b0ca5c9597cb670df9638c12f2250fe1685eb818
gabielgabateli <gabateli16@gmail.com>
2024-04-24 09:34:56 -0300

    js

--- js/carregar.js
+++ js/carregar.js
@@ -3,6 +3,8 @@ const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
 tarefas.forEach(tarefa => card(tarefa))
 
 function card (tarefa) {
+    // tem que arrumar isso aqui pra fazer adicionar e div e ficar de acordo com o CSS, ta saindo tudo na horizontal
+    // fugindo do grid
     const cardTarefa = `
     <div class="column is-3 has-text-centered">
     <h3 class="title is-4 has-text-white-ter">Academia</h3>
